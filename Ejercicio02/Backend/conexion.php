22764788ce04428f2e18a9047c3a63b9b6549064
Oswaldo Henríquez <132530276+Alexander-Henriquez@users.noreply.github.com>
2024-05-08 01:08:18 -0600

    Ejercicio 02 Alex

--- Ejercicio02/Backend/conexion.php
+++ Ejercicio02/Backend/conexion.php
@@ -1,9 +1,9 @@
 <?php
-// Datos de conexión a la base de datos
-$servername = "localhost"; // Cambia esto si tu servidor MySQL no está en localhost
-$username = "root"; // Cambia "tu_usuario" por tu nombre de usuario de MySQL
-$password = ""; // Cambia "tu_contraseña" por tu contraseña de MySQL
-$database = "sistema_boletas_pago"; // Cambia "sistema_boletas_pago" si has utilizado un nombre diferente para tu base de datos
+
+$servername = "localhost"; 
+$username = "root"; 
+$password = ""; 
+$database = "sistema_boletas_pago"; 
 
 // Crear conexión
 $conn = new mysqli($servername, $username, $password, $database);
